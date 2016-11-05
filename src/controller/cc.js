c99ebf39ce0486f26faae2f05eba28e7c7464cae
lumin824 <lumin824@163.com>
2016-11-05 22:45:59 +0800

    11

--- src/controller/cc.js
+++ src/controller/cc.js
@@ -73,23 +73,63 @@ export default class extends Base {
       });
     });
 
+    let fmjl = _.map(result.infos[2],o=>{
+      let wfss = _.find(o,o2=>_.startsWith(o2,'主要违法事实'));
+      let frxm = _.find(o,o2=>_.startsWith(o2,'法定代表人或者负责人姓名'));
+      let cfqk = _.find(o,o2=>_.startsWith(o2,'相关法律依据及处理处罚情况'));
+      let je = 0;
+      let m = cfqk.match(/(\d+(?:\.\d+)?)(元|千元|万元|十万元|百万元|千万元|亿元)/);
+      if(m){
+        je = parseFloat(m[1]);
+        if(m[2]=='千元')        je*=1e3;
+        else if(m[2]=='万元')   je*=1e4;
+        else if(m[2]=='十万元') je*=1e5;
+        else if(m[2]=='百万元') je*=1e6;
+        else if(m[2]=='千万元') je*=1e7;
+        else if(m[2]=='亿元')   je*=1e8;
+      }
+      return { wfss, cfqk, je, frxm };
+    });
+
     let schmd = _.map(result.infos[3], o=>{
+      // 最高法
       let ah = _.find(o,o2=>_.startsWith(o2,'案号'));
       let frxm = _.find(o,o2=>_.startsWith(o2,'企业法人姓名'));
       let qdyw = _.find(o,o2=>_.startsWith(o2,'法律生效文书确定的义务'));
-      let je = 0;
-      let m = qdyw.match(/(\d+(?:\.\d+)?)(元|万元)/);
-      if(m){
-        je = parseFloat(m[1]);
-        if(m[2]=='万元') je*=10000;
-      }
+      // 财政部
+      let blxw = _.find(o,o2=>_.startsWith(o2,'不良行为的具体情形'));
+      let cfjg = _.find(o,o2=>_.startsWith(o2,'处罚结果'));
 
-      return { ah, qdyw, je, frxm};
+      let je = 0;
+      let m;
+      if(qdyw && (m = qdyw.match(/(\d+(?:\.\d+)?)(元|千元|万元|十万元|百万元|千万元|亿元)/))){
+        je = parseFloat(m[1]);
+        if(m[2]=='千元')        je*=1e3;
+        else if(m[2]=='万元')   je*=1e4;
+        else if(m[2]=='十万元') je*=1e5;
+        else if(m[2]=='百万元') je*=1e6;
+        else if(m[2]=='千万元') je*=1e7;
+        else if(m[2]=='亿元')   je*=1e8;
+      }else if(cfjg && (m = cfjg.match(/(\d+(?:\.\d+)?)(元|千元|万元|十万元|百万元|千万元|亿元)/))){
+        je = parseFloat(m[1]);
+        if(m[2]=='千元')        je*=1e3;
+        else if(m[2]=='万元')   je*=1e4;
+        else if(m[2]=='十万元') je*=1e5;
+        else if(m[2]=='百万元') je*=1e6;
+        else if(m[2]=='千万元') je*=1e7;
+        else if(m[2]=='亿元')   je*=1e8;
+      }
+      return { ah, frxm, qdyw, blxw, cfjg, je};
     });
 
-    let hmdje = _.sumBy(schmd, 'je');
-    let hmddj = (Math.log(hmdje/1000) / Math.log(10)).toFixed(1);
-    this.assign({result, schmd, hmdje, hmddj});
+
+
+    let sxje = _.sumBy(fmjl, 'je') + _.sumBy(schmd, 'je');
+    let sxdj = 0;
+    if(sxje > 1000){
+      sxdj = (Math.log(sxje/1000) / Math.log(10)).toFixed(1);
+    }
+    this.assign({result, fmjl, schmd, sxje, sxdj});
     return this.display();
   }
 }
